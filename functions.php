4329615c78828f158fd584d3bb1aeb1a843d8efd
twindai <guandai@hotmail.com>
2025-04-28 01:55:03 +0200

    remove parent

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ add_theme_support('block-templates');
 function syccase_child_enqueue_styles() {
     
     // Enqueue child theme's style.css
-    wp_enqueue_style('syccase-child-style', get_stylesheet_directory_uri() . '/style.css', array('kiosko-parent-style'), wp_get_theme()->get('Version'));
+    // wp_enqueue_style('syccase-child-style', get_stylesheet_directory_uri() . '/style.css', array('kiosko-parent-style'), wp_get_theme()->get('Version'));
     
     // Enqueue parent theme's style.css
     wp_enqueue_style('kiosko-parent-style', get_template_directory_uri() . '/style.css');
