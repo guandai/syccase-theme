adf6e38900e1ae4d22018f9bd781aa49352b692d
twindai <guandai@hotmail.com>
2025-04-28 01:40:34 +0200

    update

--- functions.php
+++ functions.php
@@ -15,22 +15,22 @@ function syccase_child_gutenberg_support() {
     // Add support for custom color palette
     add_theme_support('editor-color-palette', array(
         array(
-            'name'  => __('Primary', 'storefront-child'),
+            'name'  => __('Primary', 'kiosko-child'),
             'slug'  => 'primary',
             'color' => '#0071e3',
         ),
         array(
-            'name'  => __('Secondary', 'storefront-child'),
+            'name'  => __('Secondary', 'kiosko-child'),
             'slug'  => 'secondary',
             'color' => '#005bb5',
         ),
         array(
-            'name'  => __('Light Gray', 'storefront-child'),
+            'name'  => __('Light Gray', 'kiosko-child'),
             'slug'  => 'light-gray',
             'color' => '#f9f9f9',
         ),
         array(
-            'name'  => __('Dark Gray', 'storefront-child'),
+            'name'  => __('Dark Gray', 'kiosko-child'),
             'slug'  => 'dark-gray',
             'color' => '#333',
         ),
@@ -39,22 +39,22 @@ function syccase_child_gutenberg_support() {
     // Add support for custom font sizes
     add_theme_support('editor-font-sizes', array(
         array(
-            'name' => __('Small', 'storefront-child'),
+            'name' => __('Small', 'kiosko-child'),
             'size' => 14,
             'slug' => 'small',
         ),
         array(
-            'name' => __('Normal', 'storefront-child'),
+            'name' => __('Normal', 'kiosko-child'),
             'size' => 17,
             'slug' => 'normal',
         ),
         array(
-            'name' => __('Large', 'storefront-child'),
+            'name' => __('Large', 'kiosko-child'),
             'size' => 36,
             'slug' => 'large',
         ),
         array(
-            'name' => __('Huge', 'storefront-child'),
+            'name' => __('Huge', 'kiosko-child'),
             'size' => 48,
             'slug' => 'huge',
         ),
@@ -62,5 +62,15 @@ function syccase_child_gutenberg_support() {
 }
 add_action('after_setup_theme', 'syccase_child_gutenberg_support');
 
+// Add block template support
+add_theme_support('block-templates');
 
-add_theme_support( 'block-templates' );
+// Enqueue parent and child theme styles
+function syccase_child_enqueue_styles() {
+    // Enqueue parent theme's style.css
+    wp_enqueue_style('kiosko-parent-style', get_template_directory_uri() . '/style.css');
+
+    // Enqueue child theme's style.css
+    wp_enqueue_style('syccase-child-style', get_stylesheet_directory_uri() . '/style.css', array('kiosko-parent-style'), wp_get_theme()->get('Version'));
+}
+add_action('wp_enqueue_scripts', 'syccase_child_enqueue_styles');
