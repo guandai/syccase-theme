68f4639a74b1e727ea61b5e2e64577bdc0019082
twindai <guandai@hotmail.com>
2025-04-28 03:46:56 +0200

    add template

--- functions.php
+++ functions.php
@@ -14,3 +14,22 @@ function syccase_child_enqueue_styles() {
 }
 add_action('wp_enqueue_scripts', 'syccase_child_enqueue_styles');
 
+
+
+function my_custom_homepage_template($template) {
+    if (is_front_page()) {
+        // Get the page ID of the homepage
+        $homepage_id = get_option('page_on_front');
+        
+        // Retrieve the template selected for the homepage from the page editor
+        $selected_template = get_post_meta($homepage_id, '_wp_page_template', true);
+        
+        // If a template is selected for the page, use it instead of the default
+        if ($selected_template && $selected_template !== 'front-page.php') {
+            $template = locate_template($selected_template);
+        }
+    }
+    return $template;
+}
+
+add_filter('template_include', 'my_custom_homepage_template');
---
+++ syccase-template-base.php
@@ -0,0 +1,8 @@
+<?php
+/* Template Name: syccase-template-base */
+get_header();
+?>
+
+<!-- Your custom homepage content goes here -->
+
+<?php get_footer(); ?>
