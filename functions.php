9ca4eb907e58b45c7e991d1fff5d3a738bb6f046
twindai <guandai@hotmail.com>
2025-04-28 03:33:07 +0200

    template_include

--- functions.php
+++ functions.php
@@ -13,3 +13,22 @@ function syccase_child_enqueue_styles() {
     
 }
 add_action('wp_enqueue_scripts', 'syccase_child_enqueue_styles');
+
+
+function change_homepage_template($template) {
+    if (is_front_page()) {
+        // Check if a static page is set for the homepage
+        $homepage_id = get_option('page_on_front');
+        
+        // Get the template assigned to the homepage
+        $homepage_template = get_post_meta($homepage_id, '_wp_page_template', true);
+        
+        // If the homepage template is not set to 'FrontPage', use that template
+        if ($homepage_template && $homepage_template !== 'front-page.php') {
+            $template = locate_template($homepage_template);
+        }
+    }
+    return $template;
+}
+
+add_filter('template_include', 'change_homepage_template');
