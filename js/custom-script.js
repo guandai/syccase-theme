22cc26773d87454ae8971394edd05274e94da4b5
twindai <guandai@hotmail.com>
2025-05-04 00:31:21 +0200

    comming sson

--- js/custom-script.js
+++ js/custom-script.js
@@ -20,4 +20,18 @@ document.addEventListener("DOMContentLoaded", function () {
   // Reorder the <ul>
   ul.innerHTML = "";
   items.forEach(item => ul.appendChild(item));
+
+
+
+
+  const priceContainers = document.querySelectorAll('.wc-block-grid__product-price');
+
+  priceContainers.forEach(container => {
+      const isEmpty = container.innerHTML.trim() === "";
+      if (isEmpty) {
+          container.innerHTML = '<span class="coming-soon-label">COMING SOON</span>';
+      }
+  });
 });
+
+
