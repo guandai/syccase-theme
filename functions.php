a1ee1a9ebc801736a502b2595d8caa06c3632e70
twindai <guandai@hotmail.com>
2025-05-03 23:41:52 +0200

    add sort

--- functions.php
+++ functions.php
@@ -15,3 +15,13 @@ function syccase_child_enqueue_styles() {
 add_action('wp_enqueue_scripts', 'syccase_child_enqueue_styles');
 
 
+add_filter('posts_orderby', 'sort_products_by_real_slug', 10, 2);
+function sort_products_by_real_slug($orderby, $query) {
+    if (is_admin() || !$query->is_main_query()) return $orderby;
+    
+    if ((is_shop() || is_product_category() || is_product_tag()) && is_post_type_archive('product')) {
+        global $wpdb;
+        return "{$wpdb->posts}.post_name ASC";
+    }
+    return $orderby;
+}
